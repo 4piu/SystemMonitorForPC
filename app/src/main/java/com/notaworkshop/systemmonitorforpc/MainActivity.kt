e5f96f912bf5902c9e3c98d87af6186f95fd20e9
4piu <16668056@users.noreply.github.com>
2021-12-10 03:00:11 +0800

    Add a full screen button to action bar

--- app/src/main/java/com/notaworkshop/systemmonitorforpc/MainActivity.kt
+++ app/src/main/java/com/notaworkshop/systemmonitorforpc/MainActivity.kt
@@ -2,11 +2,45 @@ package com.notaworkshop.systemmonitorforpc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.actionbar_menu, menu)
+        return true
+    }
+
+    private fun fullScreen() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        // hide action bar
+        supportActionBar?.hide()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_fullscreen -> {
+                fullScreen()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
