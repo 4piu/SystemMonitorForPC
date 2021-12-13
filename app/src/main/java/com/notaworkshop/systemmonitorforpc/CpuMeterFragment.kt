3aecb2d631c8c339722f32a13f50527fe25c227d
4piu <16668056@users.noreply.github.com>
2021-12-14 04:42:31 +0800

    Add meter chart to cpu utils

--- app/src/main/java/com/notaworkshop/systemmonitorforpc/CpuMeterFragment.kt
+++ app/src/main/java/com/notaworkshop/systemmonitorforpc/CpuMeterFragment.kt
@@ -1,5 +1,6 @@
 package com.notaworkshop.systemmonitorforpc
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,10 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.collection.CircularArray
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import org.json.JSONObject
 import java.util.*
+import kotlin.collections.ArrayList
 
 class CpuMeterFragment : Fragment(), HistoryViewer {
+    companion object {
+        private val TAG = MonitorFragment::class.qualifiedName
+    }
+    private var chart: PieChart? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +34,37 @@ class CpuMeterFragment : Fragment(), HistoryViewer {
         return inflater.inflate(R.layout.fragment_cpu_meter, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        chart = activity?.findViewById(R.id.cpu_sum_chart)
+        chart?.setUsePercentValues(true)
+        chart?.setDrawEntryLabels(false)
+        chart?.isDrawHoleEnabled = true
+        chart?.holeRadius = 85f
+        chart?.setHoleColor(Color.TRANSPARENT)
+        chart?.maxAngle = 270f
+        chart?.rotationAngle = 135f
+        chart?.legend?.isEnabled = false
+        chart?.description?.isEnabled = false
+        chart?.setTouchEnabled(false)
+    }
+
     override fun updateView(data: LinkedList<JSONObject?>, historySize: Int) {
         val percent = data.last?.getJSONObject("cpu")?.getString("percent_sum")
-        activity?.findViewById<TextView>(R.id.cpu_meter_percent_integer)?.text = if (percent==null) "---" else percent.split(".")[0]
+        // textview
+        activity?.findViewById<TextView>(R.id.cpu_meter_percent_integer)?.text = if (percent==null) "--" else percent.split(".")[0]
         activity?.findViewById<TextView>(R.id.cpu_meter_percent_fragment)?.text = if (percent==null) "-" else percent.split(".")[1]
+        // chart
+        val fPercent = percent?.toFloat()?:0F
+        val dataEntryList = arrayListOf(PieEntry(fPercent), PieEntry(100F - fPercent))
+        val dataset = PieDataSet(dataEntryList, "").apply {
+            this.setColors(
+                ContextCompat.getColor(requireContext(), R.color.teal_200),
+                ContextCompat.getColor(requireContext(),R.color.transparent_bk))
+            this.setDrawValues(false)
+        }
+        val data = PieData(dataset)
+        chart?.data = data
+        chart?.invalidate()
     }
 }
